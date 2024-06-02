436c59e68629c89f599e77d28d61a129767b69ef
kohct1 <kohct2006@gmail.com>
2024-06-02 22:43:07 +0800

    added the skeleton for the homepage and linked the buttons to their respective pages

---
+++ index.js
@@ -0,0 +1,19 @@
+function goToArcade() {
+    window.location.href = "arcade.html";
+}
+
+function goToFreePlay() {
+    window.location.href = "arcade.html";
+}
+
+function goToLeaderboard() {
+    window.location.href = "arcade.html";
+}
+
+function goToLoadGame() {
+    window.location.href = "load-game.html";
+}
+
+function exit() {
+    window.close();
+}
